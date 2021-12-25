339aa93dd8fe4b0742da285254af9c76c2d3c1ce
Александр Андрианов <kakawkawww13@mail.ru>
2021-12-25 14:29:44 +0300

    Recommit предыдущего commit

---
+++ Configurations/AppAuthenticationAndAuthorizationConfiguration.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+using WebStore.Domain.Consts;
+using WebStore.Services;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Configurations
+{
+    public static class AppAuthenticationAndAuthorizationConfiguration
+    {
+        public static IServiceCollection AddAppAuthenticationAndAuthorization(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<TokenAuthenticationStateService>();
+            services.AddScoped<AuthenticationStateProvider, TokenAuthenticationStateService>();
+            services.AddScoped<ServerAuthenticationStateProvider, TokenAuthenticationStateService>();
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Audience = configuration["JwtTokenParametrs:Audience"];
+                    options.ClaimsIssuer = configuration["JwtTokenParametrs:Issuer"];
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(configuration["JwtTokenParametrs:Secret"])),
+                        SaveSigninToken = true,
+                        RequireAudience = true,
+                        RequireExpirationTime = true,
+                        AuthenticationType = JwtBearerDefaults.AuthenticationScheme,
+                        NameClaimType = ClaimTypes.Name,
+                        RoleClaimType = ClaimTypes.Role,
+                        ValidateLifetime = true,
+                        ValidIssuer = configuration["JwtTokenParametrs:Issuer"],
+                        ValidAudience = configuration["JwtTokenParametrs:Audience"]
+                    };
+                    options.SaveToken = true;
+                    options.Validate(JwtBearerDefaults.AuthenticationScheme);
+                });
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy(PolicyConst.Guest, builder => builder.RequireAssertion(x => !x.User.Identity.IsAuthenticated));
+                options.AddPolicy(PolicyConst.Admin, bulder => bulder.RequireClaim(ClaimTypes.Role, RoleConst.Admin));
+                options.AddPolicy(PolicyConst.Moderator, bulder => bulder.RequireAssertion(x => x.User.HasClaim(ClaimTypes.Role, RoleConst.Moderator) || x.User.HasClaim(ClaimTypes.Role, RoleConst.Admin)));
+                options.AddPolicy(PolicyConst.User, bulder => bulder.RequireAssertion(x => x.User.HasClaim(ClaimTypes.Role, RoleConst.Moderator) || x.User.HasClaim(ClaimTypes.Role, RoleConst.Admin) || x.User.HasClaim(ClaimTypes.Role, RoleConst.User)));
+            });
+
+            return services;
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,21 +1,12 @@
 using Blazored.LocalStorage;
 using Blazored.SessionStorage;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Server;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
-using System.Security.Claims;
-using System.Text;
 using WebStore.Configurations;
-using WebStore.Domain.Consts;
-using WebStore.Services;
-using WebStore.Services.Interfaces;
 
 namespace WebStore
 {
@@ -32,55 +23,19 @@ namespace WebStore
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(optins => optins.Filters.Add(new AutoValidateAntiforgeryTokenAttribute()));
             services.AddServerSideBlazor();
             services.AddAdvancedDependencyInjection();
             services.AddBlazoredLocalStorage();
             services.AddBlazoredSessionStorage();
 
-            services.AddScoped<ITokenService, TokenService>();
-            services.AddScoped<TokenAuthenticationStateService>();
-            services.AddScoped<AuthenticationStateProvider, TokenAuthenticationStateService>();
-            services.AddScoped<ServerAuthenticationStateProvider, TokenAuthenticationStateService>();
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.Audience = Configuration["JwtTokenParametrs:Audience"];
-                    options.ClaimsIssuer = Configuration["JwtTokenParametrs:Issuer"];
-                    options.TokenValidationParameters = new TokenValidationParameters()
-                    {
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration["JwtTokenParametrs:Secret"])),
-                        SaveSigninToken = true,
-                        RequireAudience = true,
-                        RequireExpirationTime = true,
-                        AuthenticationType = JwtBearerDefaults.AuthenticationScheme,
-                        NameClaimType = ClaimTypes.Name,
-                        RoleClaimType = ClaimTypes.Role,
-                        ValidateLifetime = true,
-                        ValidIssuer = Configuration["JwtTokenParametrs:Issuer"],
-                        ValidAudience = Configuration["JwtTokenParametrs:Audience"]
-                    };
-                    options.SaveToken = true;
-                    options.Validate(JwtBearerDefaults.AuthenticationScheme);
-                });
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy(PolicyConst.Guest, builder => builder.RequireAssertion(x => !x.User.Identity.IsAuthenticated));
-                options.AddPolicy(PolicyConst.Admin, bulder => bulder.RequireClaim(ClaimTypes.Role, RoleConst.Admin));
-                options.AddPolicy(PolicyConst.Moderator, bulder => bulder.RequireAssertion(x => x.User.HasClaim(ClaimTypes.Role, RoleConst.Moderator) || x.User.HasClaim(ClaimTypes.Role, RoleConst.Admin)));
-                options.AddPolicy(PolicyConst.User, bulder => bulder.RequireAssertion(x => x.User.HasClaim(ClaimTypes.Role, RoleConst.Moderator) || x.User.HasClaim(ClaimTypes.Role, RoleConst.Admin) || x.User.HasClaim(ClaimTypes.Role, RoleConst.User)));
-            });
-
-            services.AddAppDbContext(Configuration);
+            services.AddAppAuthenticationAndAuthorization(configuration: Configuration);
+            services.AddAppDbContext(configuration: Configuration);
             services.AddAppValidation();
             if (Env.IsDevelopment())
                 services.AddAppMockDependencies();
 
             services.AddWMBSC();
-            services.AddHttpClient();
             services.AddHttpContextAccessor();
         }
 
