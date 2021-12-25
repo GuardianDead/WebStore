8d4578d8be38708ddc69704cd22467285470815a
Александр Андрианов <kakawkawww13@mail.ru>
2021-12-25 14:21:19 +0300

    Вынос конфигураций для авторизации и индификации в отдельные конфигурационный класс. Исправление мелькания самой страниц при заходе уже авторизированного пользователя на страницу Login/Register

---
+++ Configurations/AppAuthenticationAndAuthorizationConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using WebStore.Domain.Consts;
+using WebStore.Services;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Configurations
+{
+    public static class AppAuthenticationAndAuthorizationConfiguration
+    {
+        public static IServiceCollection AddAppAuthenticationAndAuthorization(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<TokenAuthenticationStateService>();
+            services.AddScoped<AuthenticationStateProvider, TokenAuthenticationStateService>();
+            services.AddScoped<ServerAuthenticationStateProvider, TokenAuthenticationStateService>();
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Audience = configuration["JwtTokenParametrs:Audience"];
+                    options.ClaimsIssuer = configuration["JwtTokenParametrs:Issuer"];
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(configuration["JwtTokenParametrs:Secret"])),
+                        SaveSigninToken = true,
+                        RequireAudience = true,
+                        RequireExpirationTime = true,
+                        AuthenticationType = JwtBearerDefaults.AuthenticationScheme,
+                        NameClaimType = ClaimTypes.Name,
+                        RoleClaimType = ClaimTypes.Role,
+                        ValidateLifetime = true,
+                        ValidIssuer = configuration["JwtTokenParametrs:Issuer"],
+                        ValidAudience = configuration["JwtTokenParametrs:Audience"]
+                    };
+                    options.SaveToken = true;
+                    options.Validate(JwtBearerDefaults.AuthenticationScheme);
+                });
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy(PolicyConst.Guest, builder => builder.RequireAssertion(x => !x.User.Identity.IsAuthenticated));
+                options.AddPolicy(PolicyConst.Admin, bulder => bulder.RequireClaim(ClaimTypes.Role, RoleConst.Admin));
+                options.AddPolicy(PolicyConst.Moderator, bulder => bulder.RequireAssertion(x => x.User.HasClaim(ClaimTypes.Role, RoleConst.Moderator) || x.User.HasClaim(ClaimTypes.Role, RoleConst.Admin)));
+                options.AddPolicy(PolicyConst.User, bulder => bulder.RequireAssertion(x => x.User.HasClaim(ClaimTypes.Role, RoleConst.Moderator) || x.User.HasClaim(ClaimTypes.Role, RoleConst.Admin) || x.User.HasClaim(ClaimTypes.Role, RoleConst.User)));
+            });
+
+            return services;
+        }
+    }
+}
--- Configurations/AppDbContextConfiguration.cs
+++ Configurations/AppDbContextConfiguration.cs
@@ -1,10 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Scrutor;
-using System;
-using WebStore.Data;
+﻿using WebStore.Data;
 using WebStore.Data.Entities;
 
 namespace WebStore.Configurations
@@ -13,17 +7,6 @@ namespace WebStore.Configurations
     {
         public static IServiceCollection AddAppDbContext(this IServiceCollection services, IConfiguration configuration)
         {
-            services.Scan(
-                options =>
-                {
-                    options.FromCallingAssembly()
-
-                    .AddClasses(i => i.Where(c => c.Name.EndsWith("Repository")))
-                        .UsingRegistrationStrategy(RegistrationStrategy.Skip)
-                        .AsImplementedInterfaces()
-                        .WithScopedLifetime();
-                });
-
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
             services.AddIdentity<User, Role>(options =>
             {
