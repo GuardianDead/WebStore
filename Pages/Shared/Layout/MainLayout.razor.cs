481eb3d70f203feeffdbaa8a3b5a6c75c6a45136
Александр Андрианов <kakawkawww13@mail.ru>
2022-01-13 02:24:17 +0300

    Перенос логики MainLayout в отдельный cs файл

---
+++ Pages/Shared/Layout/MainLayout.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace WebStore.Pages.Shared.Layout
+{
+    public class MainLayoutBase : LayoutComponentBase
+    {
+        [Inject] public IJSRuntime JSRuntime { get; set; }
+
+        public bool ScrollToUpIsActive { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            return JSRuntime.InvokeVoidAsync("SetMainLayouteDotnetReference", DotNetObjectReference.Create(this)).AsTask();
+        }
+
+        [JSInvokable]
+        public void ShowScrollToUp()
+        {
+            ScrollToUpIsActive = true;
+            StateHasChanged();
+        }
+        [JSInvokable]
+        public void HideScrollToUp()
+        {
+            ScrollToUpIsActive = false;
+            StateHasChanged();
+        }
+        public Task ScrollToTopAsync() => JSRuntime.InvokeVoidAsync("scrollToTop").AsTask();
+    }
+}
