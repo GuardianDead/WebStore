418165806587635363f789b15d6e220ea7e8c327
Александр Андрианов <kakawkawww13@mail.ru>
2022-01-13 02:18:59 +0300

    Создание Header, добавление Spoilers в Footer

--- Pages/Shared/Layout/Footer.razor.cs
+++ Pages/Shared/Layout/Footer.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -9,11 +10,35 @@ namespace WebStore.Pages.Shared.Layout
     {
         [CascadingParameter] public Task<AuthenticationState> AuthenticationStateTask { get; set; }
 
+        [Inject] public IJSRuntime JSRuntime { get; set; }
+
+        public ElementReference? selectedSpoilerContent;
+        public ElementReference firstSpoilerContent;
+        public ElementReference secondSpoilerContent;
+        public ElementReference thirdSpoilerContent;
+        public ElementReference fourthSpoilerContent;
         public ClaimsPrincipal currentUserState;
 
         protected override async Task OnInitializedAsync()
         {
+            await JSRuntime.InvokeVoidAsync("SetFooterDotnetReference", DotNetObjectReference.Create(this));
             currentUserState = (await AuthenticationStateTask).User;
+        }
+
+        public async Task ToogleSpoilerAsync(ElementReference spoilerContent)
+        {
+            if (selectedSpoilerContent?.Id == spoilerContent.Id)
+            {
+                await JSRuntime.InvokeVoidAsync("hideSpoilerContent", spoilerContent);
+                selectedSpoilerContent = null;
+                StateHasChanged();
+            }
+            else
+            {
+                await JSRuntime.InvokeVoidAsync("showSpoilerContent", spoilerContent);
+                selectedSpoilerContent = spoilerContent;
+                StateHasChanged();
+            }
         }
     }
 }
--- Pages/Shared/Layout/Header.razor.cs
+++ Pages/Shared/Layout/Header.razor.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.JSInterop;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
@@ -14,21 +16,40 @@ namespace WebStore.Pages.Shared.Layout
     {
         [CascadingParameter] public Task<AuthenticationState> AuthenticationStateTask { get; set; }
 
+        [Inject] public IJSRuntime JSRuntime { get; set; }
         [Inject] public AppDbContext Db { get; set; }
         [Inject] public NavigationManager NavigationManager { get; set; }
+        [Inject] public UserManager<User> UserManager { get; set; }
 
-        public List<Subcategory> subcategories;
-        public List<Category> categories = new List<Category>();
-        public Category selectedCategory;
-        public List<Subcategory> selectedSubcaregories;
+        public static bool IsSubcategoriesShow { get; set; } = false;
+        public bool IsUserPanelShow { get; set; } = false;
+        public bool IsSearchPanelActive { get; set; } = false;
+
+        public static List<Subcategory> subcategories;
+        public static List<Category> categories = new List<Category>();
+        public static Category selectedCategory;
+        public static List<Subcategory> selectedSubcaregories;
+        public string searchContent;
 
         public string returnUrl;
         public ClaimsPrincipal currentUserState;
+        public User currentUser;
 
         protected override async Task OnInitializedAsync()
         {
+            await JSRuntime.InvokeVoidAsync("SetHeaderDotnetReference", DotNetObjectReference.Create(this));
+
             returnUrl = NavigationManager.Uri.Replace('/', '$');
             currentUserState = (await AuthenticationStateTask).User;
+            if (currentUserState.Identity.IsAuthenticated)
+            {
+                var userEmail = currentUserState.Claims.Single(claim => claim.Type == ClaimTypes.Email).Value;
+                currentUser = await Db.Users
+                    .Include(user => user.Cart.Products)
+                    .Include(user => user.ListFavourites.Products)
+                    .SingleAsync(user => user.Email == userEmail);
+            }
+
             subcategories = Db.Subcategories.Include(x => x.Category).AsNoTracking().ToList();
             foreach (var subcategory in subcategories)
                 if (!categories.Any(category => category.Name == subcategory.Category.Name))
@@ -38,5 +59,68 @@ namespace WebStore.Pages.Shared.Layout
         public void NavigateToIndex() => NavigationManager.NavigateTo(NavigationManager.BaseUri, true);
         public void NavigateToLogin() => NavigationManager.NavigateTo($"{NavigationManager.BaseUri}account/authorization/login/{returnUrl}", true);
         public void NavigateToLogout() => NavigationManager.NavigateTo($"{NavigationManager.BaseUri}account/authorization/logout/{returnUrl}", true);
+
+        public int CountFavoriteProducts() => currentUser.ListFavourites.Products.Count;
+        public int CountCartProducts() => currentUser.Cart.Products.Count;
+
+        [JSInvokable]
+        public ValueTask HideSubcategoriesAsync()
+        {
+            IsSubcategoriesShow = false;
+            selectedCategory = null;
+            selectedSubcaregories = null;
+            StateHasChanged();
+            return JSRuntime.InvokeVoidAsync("hideSubcategories");
+        }
+        [JSInvokable]
+        public ValueTask ShowSubcategoriesAsync(Category category)
+        {
+            IsSubcategoriesShow = true;
+            selectedCategory = category;
+            selectedSubcaregories = subcategories.Where(subcategory => subcategory.Category.Id == category.Id).ToList();
+            StateHasChanged();
+            return JSRuntime.InvokeVoidAsync("showSubcategories");
+        }
+        [JSInvokable]
+        public async ValueTask ToggleUserPanelAsync()
+        {
+            if (IsUserPanelShow)
+            {
+                await JSRuntime.InvokeVoidAsync("hideUserPanel");
+                IsUserPanelShow = !IsUserPanelShow;
+                StateHasChanged();
+            }
+            else
+            {
+                await JSRuntime.InvokeVoidAsync("showUserPanel");
+                IsUserPanelShow = !IsUserPanelShow;
+                StateHasChanged();
+            }
+        }
+        [JSInvokable]
+        public async ValueTask ToggleSearchPanelAsync()
+        {
+            var currentWindowInnerWidth = await JSRuntime.InvokeAsync<int>("getCurrentWindowInnerWidth");
+            if (currentWindowInnerWidth <= 835)
+            {
+                if (IsSearchPanelActive)
+                {
+                    //TODO : Ищем имена тех товаров который ввел пользователь
+                }
+                else
+                {
+                    IsSearchPanelActive = true;
+                    StateHasChanged();
+                }
+            }
+
+            //TODO : Ищем имена тех товаров который ввел пользователь
+        }
+        [JSInvokable]
+        public void HideSearchPanel()
+        {
+            IsSearchPanelActive = false;
+            StateHasChanged();
+        }
     }
 }
