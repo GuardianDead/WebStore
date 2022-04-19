c89029db7b118d9644fc088c97da36c18f4218b1
Александр Андрианов <kakawkawww13@mail.ru>
2022-04-19 05:26:51 +0300

    Правда аномалии при генерации товаров для артикулов в случайном количестве

--- Data/Mocks/ProductMock.cs
+++ Data/Mocks/ProductMock.cs
@@ -2,7 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using WebStore.Data.Entities;
@@ -26,43 +25,13 @@ namespace WebStore.Data.Mocks.ProductMock
             if (await db.Products.AnyAsync(cancellationToken))
                 return true;
 
-            var productArticlesLists = new IEnumerable<ProductArticle>[]
-            {
-                db.ProductArticles
-                    .Include(productArticle => productArticle.Model.Subcategory.Category)
-                    .Where(i => i.Model.Name == "Кроссовки Nike Air Zoom Pegasus"),
-                db.ProductArticles
-                    .Include(productArticle => productArticle.Model.Subcategory.Category)
-                    .Where(i => i.Model.Name == "Ботинки для девочек adidas Terrex Trailmaker Mid R.RDY K"),
-                db.ProductArticles
-                    .Include(productArticle => productArticle.Model.Subcategory.Category)
-                    .Where(i => i.Model.Name == "Кроссовки Nike Air Zoom Pegasus"),
-                db.ProductArticles
-                    .Include(productArticle => productArticle.Model.Subcategory.Category)
-                    .Where(i => i.Model.Name == "Джинсы Levi's 514™ Straight (Big & Tall), true)"),
-                db.ProductArticles
-                    .Include(productArticle => productArticle.Model.Subcategory.Category)
-                    .Where(i => i.Model.Name == "Брюки KORPO COLLEZIONI"),
-                db.ProductArticles
-                    .Include(productArticle => productArticle.Model.Subcategory.Category)
-                    .Where(i => i.Model.Name == "Зимняя куртка мужская SHARK FORCE"),
-                db.ProductArticles
-                    .Include(productArticle => productArticle.Model.Subcategory.Category)
-                    .Where(i => i.Model.Name == "Пальто Tom Farr"),
-                db.ProductArticles
-                    .Include(productArticle => productArticle.Model.Subcategory.Category)
-                    .Where(i => i.Model.Name == "Зимняя куртка мужская SHARK FORCE"),
-                db.ProductArticles
-                    .Include(productArticle => productArticle.Model.Subcategory.Category)
-                    .Where(i => i.Model.Name == "Кепка мужская Denkor Восьмиклинка-Хулиганка"),
-                db.ProductArticles
-                    .Include(productArticle => productArticle.Model.Subcategory.Category)
-                    .Where(i => i.Model.Name == "Шляпа ARMANI"),
-            };
-
-            IEnumerable<Product> products = productArticlesLists
-                .SelectMany(productArticles => productArticles
-                .SelectMany(article => Enumerable.Repeat(new Product(article), random.Next(50, 101))));
+            List<ProductArticle> productArticles = await db.ProductArticles
+                .Include(productArticle => productArticle.Model.Subcategory.Category)
+                .ToListAsync();
+            List<Product> products = new List<Product>();
+            foreach (var productArticle in productArticles)
+                for (int i = 0; i < random.Next(11, 22); i++)
+                    products.Add(new Product(productArticle));
 
             foreach (Product product in products)
                 await productValidator.ValidateAndThrowAsync(product, cancellationToken);
